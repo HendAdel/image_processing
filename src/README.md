519311babcf558b1f344508839f0b57ed58d8362
HendAdel <hend.adel.m@gmail.com>
2022-03-23 23:19:14 +0200

    Install sharp, make the endpoint, add readme file.

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# Processing Images Project
+> It is a simple project to resize the image by user inputes for selected image and the new width and height he enter them..
+
+# Website Feture
+## Images Gallery
+> Show all existing images, to choose one of them to change it's size.
+
+## In this project I did:
+* Using node.js, typeScript
+* Using prettier with eslint
+* The server that have the endpoint function.
+* Using Jasmine for test unit.
+* Use sharp to resize the image.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
+// import the express to build the server
 import express = require('express');
+//call sharp tool to resize image
+const sharp = require('sharp');
+
 const app = express();
 
 const port = 3000;
 
+// resize image endpoint function
 app.get('/',(req, res) => {
-    res.send("Hi the server response!");
+    sharp(req)
+  .resize(req.query.wdith, req.query.height, {
+    kernel: sharp.kernel.nearest,
+    fit: 'contain',
+    position: 'right top',
+    background: { r: 255, g: 255, b: 255, alpha: 0.5 }
+  })
+  .toFile(`${req.query.fname}.jpg`)
+  .then(() => {
+    
+  });
+    res.send("Image size changed!");
 });
 
+// start server function
 app.listen(port, ()=>{
     console.log(`The server strted at localhost: ${port}`);
 })
